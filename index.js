f9445d5da1b85190574fc9ddcee9a380044376df
morisisi89 <113606870+morisisi89@users.noreply.github.com>
2023-02-08 12:45:53 +0100

    build : allback function

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Hello Node.js!")
+const serverHandle = function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+  }
+  
+  const server = http.createServer(serverHandle);
+  server.listen(8080)
