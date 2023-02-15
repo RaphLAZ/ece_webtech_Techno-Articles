40214d9470e714c631f89eaa74d076903ba9fcc1
morisisi89 <113606870+morisisi89@users.noreply.github.com>
2023-02-15 11:12:39 +0100

    Build : lab 2 part 5 create multiple route

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # WebTech-ECE-ING4
+
+## Introduction :
+* Web technology project for ECE.
+* Group : Axel PAPE & Raphael LAZZARI-ARMOUR
+* Teachers : M. FARAULT PAUL
+
+* Original instructions git : https://github.com/adaltas/ece-webtech-2023-spring-app 
+
--- handles.js
+++ handles.js
@@ -1,23 +1,86 @@
-// Import a module
-const http = require('http')
-// Import Node url module
-const url = require('url')
-const qs = require('querystring')
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const port = 8080;
 
-module.exports = {
-  serverHandle: function (req, res) {
-    const route = url.parse(req.url)
-    const path = route.pathname 
-    const params = qs.parse(route.query)
+const explication = '<!DOCTYPE html>' +
+'<html>' +
+'    <head>' +
+'        <meta charset="utf-8" />' +
+'        <title>Web Tech Project App</title>' +
+'    </head>' +
+'    <body>' +
+'       <h1>How to use /hello route</h1>' +
+'       <p>To use the /hello route, add a name query parameter to the URL like this:</p>' +
+'       <pre>/hello?name=YourName</pre>' +
+'       <p>For example:</p>' +
+'       <pre>/hello?name=John</pre>' +
+'       </body>' +
+'</html>'
 
-    res.writeHead(200, {'Content-Type': 'text/plain'});
+const presentation = '<!DOCTYPE html>' +
+'<html>' +
+'    <head>' +
+'        <meta charset="utf-8" />' +
+'        <title>Web Tech App</title>' +
+'    </head>' +
+'    <body>' +
+'       <h1>Welcome to the biographie page</h1>' +
+'       <p>We are Raphael and Axel, two students in apprentice at ECE Paris Engineering school. We are both doing this project regarding the web technology course we are both following for this semester.</p>' +
+'       </body>' +
+'</html>'
 
-    if (path === '/hello' && 'name' in params) {
-    	res.write('Hello ' + params['name'])
-    } else {
-    	res.write('Hello anonymous')
-      }
-      
+const server = http.createServer((req, res) => {
+const parsedUrl = url.parse(req.url, true);
+const path = parsedUrl.pathname;
+const query = parsedUrl.query;
+
+  //Dans le cas où l'url est juste http://localhost:8080/
+  if (path === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(explication);
     res.end();
-    } 
-}
+  }
+
+  //Dans le cas où l'url est http://localhost:8080/hello?name=NAME
+  else if (path === '/hello') {
+    const name = query.name;
+    //Dans le cas où le champ "name" n'est pas rempli
+    if (!name) {
+      res.writeHead(400, { 'Content-Type': 'text/plain' });
+      res.write('Please provide a name query parameter');
+      res.end();
+      return;
+    }
+    //Dans le cas où le champ "name" est celui d'un du binôme
+    else if (name === 'Raphael' || 'Axel') {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.write(presentation);
+      res.end();
+    }
+    else {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.write(`Hello ${name}!`);
+      res.end();
+    }
+  }
+
+  //Dans le cas où l'url est http://localhost:8080/about
+  else if (path === '/about') {
+    const file = require('./content/about.json')
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.write(JSON.stringify(file));
+    res.end();
+  }
+
+  //Affiche une erreur 404 si on est dans aucun cas de figure précèdent
+  else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.write('404 Not Found');
+    res.end();
+  }
+});
+
+server.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
