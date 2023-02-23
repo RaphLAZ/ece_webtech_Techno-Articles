ec95ef5675bad62ad3182d9db975ed9e695d9950
RaphLAZ <raphaelarmourlazzari@gmail.com>
2023-02-23 10:01:13 +0100

    patch: headers and footer added for lab4, need to test

---
+++ Labs/Lab4/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; Raphael LAZZARI-ARMOUR</p>
+    </footer>
+  );
+}
+
+export default Footer;
+
---
+++ Labs/Lab4/pages/contacts.jsx
@@ -0,0 +1,18 @@
+// pages/contacts.js
+import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+function Contacts() {
+  return (
+    <div>
+      <Header/>
+      <h1>Contact Page</h1>
+      <p>Get in touch with us.</p>
+      <Footer/>
+    </div>
+  );
+}
+
+export default Contacts;
+
